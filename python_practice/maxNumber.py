c41e3dfea3b544faf6ab00b7e441d892bf75077d
inh556 <Yanbin@Yanbins-MacBook-Pro.local>
2017-05-28 08:52:09 -0700

    a new one

--- python_practice/maxNumber.py
+++ python_practice/maxNumber.py
@@ -1,14 +1,11 @@
-print 'input three numbers'
-num1 = input()
-num2 = input()
-num3 = input()
-if num1 > num2:
-    if num1 > num3:
-        print num1
-    else:
-        print num3
-if num1 < num2:
-    if num2 > num3:
-        print num2
-    else:
-        print num3
+
+num1 = input('input num1')
+num2 = input('input num2')
+num3 = input('input num3')
+max_num = num1
+if num2 > max_num:
+    max_num = num2
+if num3 > max_num:
+    max_num = num3
+print max_num
+
